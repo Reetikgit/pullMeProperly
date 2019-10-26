4d99c96e281f0ac978c4ddde91ba90e6c971cb0b
Reetik Chitragupt <39235061+Reetikgit@users.noreply.github.com>
2019-10-26 16:48:32 +0530

    Update HitMe.java

--- HitMe.java
+++ HitMe.java
@@ -5,10 +5,19 @@ class HitMe {
     int a = 10;
     int b = 20;
     int sum = add(a,b);
+    int diff = sub(a,b);
     System.out.println(sum);
+    System.out.println(diff);
+
   }
 
   public static int add(int a, int b) {
     return a+b;
+    return a-b; 
+  }
+  
+  public static int sub(int a, int b) {
+    
+    return a-b; 
   }
 }
